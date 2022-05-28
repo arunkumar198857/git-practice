240e2eb839fc086e1f69a75e8b3bafd02ca6b7b3
arunkumar198857 <arunkumar198857@gmail.com>
2022-05-28 18:49:22 +0530

    added content in componentnew

--- src/ComponentNew.js
+++ src/ComponentNew.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ComponentNew = () => {
-  return <div>ComponentNew</div>;
+  return (
+    <div>
+      <h1>Adding heading</h1>
+      <p>Adding paragraph</p>
+    </div>
+  );
 };
 
 export default ComponentNew;
