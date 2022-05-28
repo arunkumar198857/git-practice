0d2deedbecd460f94da6df31e07598dd9a7b2402
arunkumar198857 <arunkumar198857@gmail.com>
2022-05-28 18:45:18 +0530

    added new file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ComponentNew from "./ComponentNew";
 function App() {
   return (
    <div className="main">
      <h1>Hello world!</h1>
      <p>This is a create react app.</p>
+     <ComponentNew />
    </div>
   );
 }
